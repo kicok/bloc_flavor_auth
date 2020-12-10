5218c348c6dd1e116bf33ffab25f6e084207b9e9
kicok <kicoker@gmail.com>
2020-12-10 18:29:00 +0900

    change index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const authenticate = (req, res, next) =>{
 
         let token;
         if(req.headers.authorization &&
-            req.headers.authenticate.startWith('Bearer')){
+            req.headers.authorization.startsWith('Bearer')){
                 token = req.headers.authorization.split(' ')[1];
             }
 
